8815bc5bb6896753609a27a8d4e02384371355db
Nayef Ghattas <nayef.ghattas@datadoghq.com>
2024-06-10 15:50:30 +0200

    dotfiles: keep zshrc

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ ln -sf $HOME/dotfiles/.gitconfig $HOME/.gitconfig
 ln -sf $HOME/dotfiles/.zshrc $HOME/.zshrc
 
 # Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --keep-zshrc
 
 # Install powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k

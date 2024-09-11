0cea7231039a33aa0889cbf424c85a80131663c7
Nayef Ghattas <nayef.ghattas@datadoghq.com>
2024-09-11 11:02:12 +0200

    install: create directory if it doesn't exist

--- install.sh
+++ install.sh
@@ -14,6 +14,7 @@ sudo apt-get -qq --yes install silversearcher-ag htop net-tools tig
 POWERLEVEL10K_DIR="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 
 if [ ! -d "${POWERLEVEL10K_DIR}" ]; then
+  mkdir -p "${POWERLEVEL10K_DIR}"
   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${POWERLEVEL10K_DIR}"
 fi
 
@@ -24,6 +25,7 @@ rm -rf "${POWERLEVEL10K_DIR}/gitstatus"
 ZSH_SYNTAX_DIR="${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
 
 if [ ! -d "${ZSH_SYNTAX_DIR}" ]; then
+  mkdir -p "${ZSH_SYNTAX_DIR}"
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_SYNTAX_DIR}"
 fi
 

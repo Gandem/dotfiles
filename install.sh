6250b9b10c5c7ab037fb75f1377f7e705b16ca74
Nayef Ghattas <nayef.ghattas@datadoghq.com>
2024-11-18 15:38:42 +0100

    install.sh: install coreutils

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ ln -sf $HOME/dotfiles/.zshrc $HOME/.zshrc
 
 # Install packages
 sudo apt-get -qq update
-sudo apt-get -qq --yes install silversearcher-ag htop net-tools tig fzf
+sudo apt-get -qq --yes install silversearcher-ag htop net-tools tig fzf coreutils
 
 # Install powerlevel10k
 POWERLEVEL10K_DIR="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
